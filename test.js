53791dc801ce5e28f35a92aa839d41d3c1d4a3ae
John Hady <johnhady101@icloud.com>
2026-08-23 01:55:12 +0300

    adding k6 test

---
+++ test.js
@@ -0,0 +1,11 @@
+import http from 'k6/http';
+
+export const options = {
+    vus: 50,
+    duration: '500s',
+}
+
+export default () => {
+    // http.get('http://54.145.145.235:8501');
+    http.get('http://172.31.46.143:8501');
+};
